45238535a60604933c6b5fffdc6caee0e0acb8c2
safaa-hamdy <safaahamdy258@gmail.com>
2022-07-22 05:11:09 +0200

    sonarqube bugs fixed

--- trainingportal/aescrypto.js
+++ trainingportal/aescrypto.js
@@ -1,36 +1,35 @@
-var crypto = require('crypto');
-var util = require('util');
+let crypto = require('crypto');
+let util = require('util');
 
 function getEncParams(keySeed,ivSeed){
-    var myKeySeed = keySeed;
-    var myIvSeed = ivSeed;
+    let myKeySeed = keySeed;
+    let myIvSeed = ivSeed;
     if(util.isNullOrUndefined(myKeySeed) || util.isNullOrUndefined(myIvSeed)){
-       var myKeySeed = process.env.ENC_KEY;
-       var myIvSeed = process.env.ENC_KEY_IV;
+        myKeySeed = process.env.ENC_KEY;
+        myIvSeed = process.env.ENC_KEY_IV;
     }
-    var cryptkey = crypto.createHash('sha256').update(myKeySeed).digest();
-    var iv = crypto.createHash('sha256').update(myIvSeed).digest().slice(0,16);
+    let cryptkey = crypto.createHash('sha256').update(myKeySeed).digest();
+    let iv = crypto.createHash('sha256').update(myIvSeed).digest().slice(0,16);
 
     return {key:cryptkey, iv:iv};
 }
 
 exports.decrypt = function(encryptdata, keySeed, ivSeed) {
-    var keyParams = getEncParams(keySeed,ivSeed);
+    let keyParams = getEncParams(keySeed,ivSeed);
     encryptdata = Buffer.from(encryptdata, 'base64').toString('binary');
-    var decipher = crypto.createDecipheriv('aes-256-cbc', keyParams.key, keyParams.iv);
-    var decoded  = decipher.update(encryptdata, 'binary', 'utf8');
+    let decipher = crypto.createDecipheriv("AES-128-CBC", keyParams.key, keyParams.iv);
+    let decoded  = decipher.update(encryptdata, 'binary', 'utf8');
     decoded += decipher.final('utf-8');
     return decoded;
 }
 
 exports.encrypt = function(cleardata, keySeed, ivSeed) {
-    var keyParams = getEncParams(keySeed,ivSeed);
+    let keyParams = getEncParams(keySeed,ivSeed);
 
-    var encipher = crypto.createCipheriv('aes-256-cbc', keyParams.key, keyParams.iv);
-    var encryptdata  = encipher.update(cleardata, 'utf8', 'binary');
+    let encipher = crypto.createCipheriv("AES-256-GCM", keyParams.key, keyParams.iv);
+    let encryptdata  = encipher.update(cleardata, 'utf8', 'binary');
 
     encryptdata += encipher.final('binary');
-    let encode_encryptdata = Buffer.from(encryptdata, 'binary').toString('base64');
-    return encode_encryptdata;
+    return Buffer.from(encryptdata, 'binary').toString('base64');
 }
 
